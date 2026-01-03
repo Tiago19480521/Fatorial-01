0ebc19c656cfb22d791b84d196b610430eba875d
Tiago19480521 <146497077+Tiago19480521@users.noreply.github.com>
2026-01-03 01:35:36 -0300

    Quarto registro

--- Fatorial_01.js
+++ Fatorial_01.js
@@ -1,4 +1,28 @@
-let num = parseFloat(document.getElementById('inp_num').value)
-let fat = document.getElementById('inp_fat').value
+window.onload = function() {
+    document.getElementById("inp_num").focus();
+}
 
-document.getElementById('inp_num').innerHTML = num
+const dispResults = ()=> {
+    let num = parseFloat(document.getElementById('inp_num').value)
+    document.getElementById('inp_num1').value = num.toLocaleString('pt-BR')
+    let fat = calcFatorial(num)
+    document.getElementById('inp_fat').value = fat.toLocaleString('pt-BR')
+}
+
+const calcFatorial = (n) => {
+    // num = n * (n - 1) * (n - 2) * (n - 3) * (n - 4)
+    m = n
+    for (let i = 1; i < n; i++)  {        
+        m = m * (n - i)
+    }
+
+    return m
+}
+
+const limpaCampos = () => {
+    document.getElementById('inp_num').value = ""
+    document.getElementById('inp_num1').value = ""
+    document.getElementById('inp_fat').value = ""
+
+    document.getElementById("inp_num").focus();
+}
